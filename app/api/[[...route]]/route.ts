1485e128e30c6216b88c4fa31aeeea56976b0145
dimaluz <dimaluz@rambler.ru>
2024-05-27 17:27:40 +0100

    Hono setup was built

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,3 +1,7 @@
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
@@ -5,11 +9,16 @@ export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
-    return c.json({
-        message: 'hello'
+app
+    .get('/hello', clerkMiddleware(),(context) => {
+        const auth = getAuth(context)
+        if (!auth) {
+            return context.json({error: "Unauthorized"})
+        }
+        return context.json({
+            message: 'hello'
+        })
     })
-})
 
 export const GET = handle(app)
 export const POST = handle(app)
